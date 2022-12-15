d6b4d2a9476793fdb0f021fb5862eddbe7d6f86e
AzamaT_S <asultangareev@gmail.com>
2022-12-15 13:13:29 +0300

    Init

---
+++ src/main/java/com/example/SpringBootAppWithRedisCache/SpringBootAppWithRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAppWithRedisCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppWithRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppWithRedisCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootAppWithRedisCache/SpringBootAppWithRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAppWithRedisCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppWithRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
